51acad6336301f7cb402eeb2b3ab21942e367046
Muhammad Rafi <orafi202@gmail.com>
2023-05-08 04:01:46 +0500

    update

---
+++ Exercises-Q1/Exe20.ts
@@ -0,0 +1,8 @@
+/*
+Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything
+else you’d like. Write a program that creates a list containing these items.
+*/
+
+let cricketTeam: string[] = ['Pakistan', 'India', 'Afghanistan', 'England', 'Bangladash', 'South Africa', 'Austerlia', 'New Zealand'];
+
+console.log("Cricket Teams are: ", cricketTeam);
---
+++ Exercises-Q1/Exe21.ts
@@ -0,0 +1,19 @@
+/*21. They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.*/
+
+let Student = {
+    id: 1,
+    name: "Muhammad Rafi",
+    age: 32,
+    city: "Lahore"
+};
+
+let Employee = {
+
+    employeeId:16331,
+    employeeName: 'Muhammad Rafi',
+    DeptName: 'Contact Center',
+    Designation: 'Manager'
+};
+
+console.log(Employee);
+console.log(Student);
