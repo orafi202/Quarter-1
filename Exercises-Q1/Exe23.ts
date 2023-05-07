e1b167a32018810dc67de0f1ba8ffc371de4848c
Muhammad Rafi <orafi202@gmail.com>
2023-05-08 04:10:53 +0500

    exercises

---
+++ Exercises-Q1/Exe23.ts
@@ -0,0 +1,28 @@
+/*
+Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+
+*/
+
+let age = 20;
+if( age <= 20) {
+    console.log("Sorry! You're minor.");
+} else {
+    console.log('You are adult. ');
+}
+
+let isSunday = 'sunday';
+let currentDay = 'monday';
+
+if( isSunday === currentDay) {
+    console.log("let\'s go for the party.");
+} else {
+    console.log("No! i have to goto office.");
+}
+
